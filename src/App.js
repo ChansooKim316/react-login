5d2c81de1d0e0b0f28f497c24c81122478916588
ChansooKim316 <cccssk@naver.com>
2021-10-07 10:07:25 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,48 +1,62 @@
+import React from 'react';
 import Login from './components/Login/Login';
 import Page1 from './components/Page_1/Page_1';
 import Page2 from './components/Page_2/Page_2';
 import Sidebar from "./components/Sidebar/Sidebar";
+import {Component} from "react";
 import "./App.css"
+
 
 const initialState = {
     isLoggedin: false,
-    currPage: 'page1'
+    currPage: null
 }
 
 
-function App() {
-  console.log('CURRENT STATE :', initialState.isLoggedin)
-  if (!initialState.isLoggedin) {
-    return <Login />
-  }
-  const currPage = initialState.currPage;
-  switch (currPage) {
-      case 'page1' :
-          return (
-              <div className="container">
-                  <div className="sideBar">
-                      <Sidebar/>
-                  </div>
-                  <div className="page1">
-                      <Page1 />
-                  </div>
-              </div>
-          )
-      case 'page2' :
-          return (
-              <div className="container">
-                  <div className="sideBar">
-                      <Sidebar/>
-                  </div>
-                  <div className="page2">
-                      <Page2 />
-                  </div>
-              </div>
-          )
-      // 이후 다른 페이지들 추가 가능
-      default :
-          return '<h3>Something went wrong. Please try again</h3>'
-  }
+// function App() {
+class App extends Component {
+    constructor() {
+        super();
+        this.state = initialState;
+    }
+
+    render() {
+        const currPage = initialState.currPage;
+        console.log('CURRENT STATE :', initialState);
+        if(initialState.isLoggedin) {
+            console.log('NOW LOGGED IN !!!')
+        }
+        if (!initialState.isLoggedin) {
+            return <Login />
+        }
+        switch (currPage) {
+            case 'page1' :
+                return (
+                    <div className="container">
+                        <div className="sideBar">
+                            <Sidebar/>
+                        </div>
+                        <div className="page1">
+                            <Page1 />
+                        </div>
+                    </div>
+                );
+            case 'page2' :
+                return (
+                    <div className="container">
+                        <div className="sideBar">
+                            <Sidebar/>
+                        </div>
+                        <div className="page2">
+                            <Page2 />
+                        </div>
+                    </div>
+                )
+            // 이후 다른 페이지들 추가 가능
+            default :
+                return '<h3>Something went wrong. Please try again</h3>'
+        }
+    }
 }
 
 export default App;
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,28 +2,43 @@ import React, { Component } from 'react'
 import './Login.css'
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: '',
+            password: '',
+            isLoggedin: false
+        }
+    }
     // setState
-    submitSignIn = () => {
-
+    onIdChange = (event) => {
+        this.setState({id: event.target.value})
+    }
+    onPasswordChange = (event) => {
+        this.setState({password: event.target.value})
+    }
+    submitLogIn = () => {
+        this.setState({isLoggedin: true})
+        // App.js 로 this.state 전달
+        // ??
     }
 
-    // enter key listener -> 엔터 안먹힐 경우
-
     render() {
+        console.log('STATE :', this.state)
         return(
             <div>
                 <h3> LOGIN PAGE</h3>
                 <div className="id" >
                     <b>ID : </b>
-                    <input type="id" id="id-input" placeholder="ckstack"/>
+                    <input type="id" id="id-input" placeholder="ckstack" onChange={this.onIdChange}/>
                 </div>
                 <div className="password">
                     <b>PW : </b>
-                    <input type="id" id="id-input" placeholder="1234"/>
+                    <input type="id" id="id-input" placeholder="1234" onChange={this.onPasswordChange}/>
                 </div>
 
                 <div className="submit">
-                    <input type="submit" value="submit" />
+                    <input type="submit" value="submit" onClick={this.submitLogIn} />
                 </div>
             </div>
         )
