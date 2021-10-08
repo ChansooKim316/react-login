7af6948eb6a88bddd63b5923ce73fa6562fcace4
ChansooKim316 <cccssk@naver.com>
2021-10-08 10:52:57 +0900

    .

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,29 +1,63 @@
 import React, { Component } from "react";
 
+
 class Table extends Component {
     render() {
         const filteredUserData = this.props;
         console.log('GIVEN USER DATA :', filteredUserData)
+
+       const userDataToTable = (data) => {
+           console.log('func called')
+           // 항목들 (첫째줄)
+           const title =
+               <tr>
+                   <th>번호</th>
+                   <th>이름</th>
+                   <th>나이</th>
+                   <th>성별</th>
+               </tr>
+           // 요소들
+           const items =
+           <tr>
+               <td>data[0].id</td>
+               <td>data[0].name</td>
+               <td>data[0].age</td>
+               <td>data[0].sex</td>
+           </tr>
+           <tr>
+               <td>data[1].id</td>
+               <td>data[1].name</td>
+               <td>data[1].age</td>
+               <td>data[1].sex</td></tr>
+
+
+           return <div><table>{title}{items}</table></div>
+        }
         return (
+
             <div>
                 TABLE COMPONENT
-                <p>----- table -----</p>
-                <table>
-                    <tr>
-                        <th>번호</th>
-                        <th>이름</th>
-                        <th>나이</th>
-                        <th>성별</th>
-                    </tr>
-                    {filteredUserData.map(data => (
-                        <tr>
-                            <td>{data.id}</td>
-                            <td>{data.name}</td>
-                            <td>{data.age}</td>
-                            <td>{data.sex}</td>
-                        </tr>
-                    ))}
-                </table>
+                {userDataToTable()}
+                {/*<table>*/}
+                {/*    <tr>*/}
+                {/*        <th>번호</th>*/}
+                {/*        <th>이름</th>*/}
+                {/*        <th>나이</th>*/}
+                {/*        <th>성별</th>*/}
+                {/*    </tr>*/}
+                {/*    <tr>*/}
+                {/*        <td>data[0].id</td>*/}
+                {/*        <td>data[0].name</td>*/}
+                {/*        <td>data[0].age</td>*/}
+                {/*        <td>data[0].sex</td>*/}
+                {/*    </tr>*/}
+                {/*    <tr>*/}
+                {/*        <td>data[1].id</td>*/}
+                {/*        <td>data[1].name</td>*/}
+                {/*        <td>data[1].age</td>*/}
+                {/*        <td>data[1].sex</td>*/}
+                {/*    </tr>*/}
+                {/*</table>*/}
             </div>
         )
     }
